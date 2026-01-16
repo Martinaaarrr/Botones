4e6af7a8f5b2596e28aef496001425108e032e70
Martinaaarrr <mrodriguezr10@educantabria.es>
2026-01-16 13:44:04 +0100

    Add files via upload

---
+++ botonBasico-main/code/boton.js
@@ -0,0 +1,14 @@
+const hazAlgo = document.getElementById("elBoton");
+
+hazAlgo.onclick = function () {
+ document.body.style.backgroundColor = "#66ff7aff";
+}
+const hazOtraCosa = document.getElementById("otroBoton");
+
+hazOtraCosa.onclick = function () {
+ document.body.style.backgroundColor = "#876ce7ff";
+}
+const hazunaterceraCosa = document.getElementById("tercerBoton");
+hazunaterceraCosa.onclick = function () {
+ document.body.style.backgroundColor = "rgb(204, 243, 64)";
+}
